a9b2f2df81ffa8dda1117d0c44e7726fa4226c8f
iyad <imoucharrafie@gmail.com>
2025-02-08 19:00:21 -0400

    close

---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <main>
+    <div class="flex justify-center items-center mt-8">
+    <h1 class="text-4xl text-blue-900">
+    Rick and Morty
+    </h1>
+  </div>
+
+
+  <div class="grid grid-cols-6 gap-4 mt-24 mx-10">
+    <div v-for="character in characters" :key="character.id" class="rounded-2xl overflow-hidden shadow-2xl">
+      <img :src="character.image" alt="character.name">
+      <div class="mt-4 text-center ">
+      {{ character.name }}
+    </div>
+    </div> 
+    </div>
+
+  </main>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const characters: any = ref([])
+
+const loadCharacters = async () => {
+  const response = await fetch('https://rickandmortyapi.com/api/character')
+  const data = await response.json()
+  characters.value = data.results
+}
+
+
+onMounted(() => {
+  loadCharacters()
+})
+</script>
+
+
+<style>
+
+
+
+</style>
