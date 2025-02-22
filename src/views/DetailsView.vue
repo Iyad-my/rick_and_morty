0b0fffa9a01fe629f2ff917ad8089a99e6b7f78a
iyad <imoucharrafie@gmail.com>
2025-02-22 18:22:03 -0400

    searcbar

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,5 +1,22 @@
 <template>
- hi
+  <div class="flex justfy-center items-center h-screen">
+  <div class="grid grid-cols-1 lg:grid-cols-2 mt-8 lg:w-6xl" v-if="character">
+    <div class="flex justify-center">
+      <img class="rounded-2xl w-96" :src="character.image" :alt="character.name">
+    </div>
+
+    <div class="flex flex-col items-center mx-auto mt-8 shadow-2xl p-4 rounded-2xl w-xl">
+      <h1 class="text-3xl font-bold mb-4">{{character.name}}</h1>
+      <div class=" lg:space-y-4 text-xl">
+      <p><b>Ubicacion:</b>{{character.location.name}}</p>
+      <p><b>Origen:</b>{{character.origin.name}}</p>
+      <p><b>Estado:</b>{{character.status}}</p>
+      <p><b>Especie:</b>{{character.species}}</p>
+      <p><b>Genero:</b>{{character.gender}}</p>
+    </div>
+  </div>
+</div>
+</div>
 </template>
 
 <script setup>
@@ -15,7 +32,7 @@ const loadCharacters = async () => {
   const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
   const data = await response.json()
   console.log(data)
-  characters.value = data
+  character.value = data
   console.log(character.value)
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,15 +6,26 @@
     </h1>
   </div>
 
-  <div class="flex justify-center space-x-4 mt-8">
-  <button class="border-2 border-black rounded-full p-1 " @click="decrementPage">-</button>
+
+  <input class="border-2 border-amber-700" v-model="searchName" type="text">
+  <button 
+  v-for="status in statusFilters"
+  :class="['bg-black text-white p-2 rounded-full', searchStatus === status ? 'bg-red-500' : 'bg-black' ]"
+  @click="searchStatus(status)"
+  >
+  {{ status }}
+</button>
+
+space-x-4 mt-8">
+  <button class="border-2 borde
+  <div class="flex justify-center r-black rounded-full p-1 " @click="decrementPage">-</button>
 <div> {{ page }}</div>
 <button class="border-2 border-black rounded-full p-1 " @click="incrementPage">+</button>
 </div>
 
 
   <div class="grid grid-cols-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-7 mt-24 mx-10">
-    <div  v-for="character in characters" :key="character.id"  class="rounded-2xl overflow-hidden shadow-2xl">
+    <div  v-for="character in filteredCharacters" :key="character.id"  class="rounded-2xl overflow-hidden shadow-2xl">
       <div @click="seeCharacterDetails(character.id)">
       <img :src="character.image" alt="character.name">
       <div class="mt-4 text-center ">
@@ -26,19 +37,37 @@
   </main>
 </template>
 
+
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const characters: any = ref([])
+const searchName = ref ('')
+const searchStatus = ref('All')
 const page:any = ref(1)
+
+const statusFilters = ['All','Alive','Dead','Unknown']
+
+const filteredCharacters = computed(()=>{
+return characters.value.filter((character: any)=>{
+  const matchName = character.name.toLowerCase().includes(searchName.value.toLowerCase())
+  return matchName
+  })
+})
+const searchByStatus = (status:string)=>{
+  searchStatus.value = status
+  console.log(status)
+}
+
 
 const loadCharacters = async () => {
   const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page.value}`)
   const data = await response.json()
   characters.value = data.results
 }
+
 
 const incrementPage =()=>{
   page.value++
@@ -56,8 +85,6 @@ const seeCharacterDetails =(character_id: number)=>{
   router.push(`/details/${character_id}`)
 
 }
-
-
 
 onMounted(() => {
   loadCharacters()
